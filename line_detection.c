cef51ef530d9bd16a35a777e3b96363bdc5d770b
Iliane Formet <iliane.formet@epita.fr>
2023-02-16 17:23:18 +0100

    Start

---
+++ line_detection.c
@@ -0,0 +1,13 @@
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <mat.h>
+
+// TOOLS
+
+// PART 1: CANNY EDGE
+//
+//STEP 1: GAUSSIAN BLUR
+
+SDL_Surface* gaussian_blur(SDL_Surface* surface){
+
+}
